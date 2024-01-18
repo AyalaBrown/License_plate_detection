5869ddad03c487e8e785c5ca67412c7af814f2c2
Ayala Brown <Bayala@isrtech.co.il>
2024-01-18 12:48:48 +0200

    18/01/2024

---
+++ util.py
@@ -0,0 +1,187 @@
+import string
+import easyocr
+
+# Initialize the OCR reader
+reader = easyocr.Reader(['en'], gpu=False)
+
+# Mapping dictionaries for character conversion
+dict_char_to_int = {'O': '0',
+                    'I': '1',
+                    'J': '3',
+                    'A': '4',
+                    'G': '6',
+                    'S': '5',
+                    'Z': '2'}
+
+dict_int_to_char = {'0': 'O',
+                    '1': 'I',
+                    '3': 'J',
+                    '4': 'A',
+                    '6': 'G',
+                    '5': 'S',
+                    '2': 'Z'}
+
+
+def write_csv(results, output_path):
+    """
+    Write the results to a CSV file.
+
+    Args:
+        results (dict): Dictionary containing the results.
+        output_path (str): Path to the output CSV file.
+    """
+    with open(output_path, 'w') as f:
+        f.write('{},{},{},{}\n'.format('license_plate_bbox', 'license_plate_bbox_score', 'license_number',
+                                                'license_number_score'))
+        if 'license_plate' in results.keys() and \
+            'text' in results['license_plate'].keys():
+            f.write('{},{},{},{}\n'.format('[{} {} {} {}]'.format(
+                                                results['license_plate']['bbox'][0],
+                                                results['license_plate']['bbox'][1],
+                                                results['license_plate']['bbox'][2],
+                                                results['license_plate']['bbox'][3]),
+                                            results['license_plate']['bbox_score'],
+                                            "'{}'".format(results['license_plate']['text']),
+                                            results['license_plate']['text_score'])
+                    )
+        f.close()
+
+
+def license_complies_format(text):
+    """
+    Check if the license plate text complies with the required format.
+
+    Args:
+        text (str): License plate text.
+
+    Returns:
+        bool: True if the license plate complies with the format, False otherwise.
+    """
+    if len(text) != 7 and len(text) !=8:
+        return False
+
+    print("license_complies_format", text)
+    print(len(text))
+    if len(text) == 8:
+        if (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
+            (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
+            (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
+            (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in dict_char_to_int.keys()) and \
+            (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
+            (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
+            (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
+            (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()):
+            return True
+        return False
+    if len(text) == 7:
+        if (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
+            (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
+            (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
+            (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in dict_char_to_int.keys()) and \
+            (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
+            (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
+            (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()):
+            return True
+        return False
+
+
+def format_license(text):
+    """
+    Format the license plate text by converting characters using the mapping dictionaries.
+
+    Args:
+        text (str): License plate text.
+
+    Returns:
+        str: Formatted license plate text.
+    """
+    if len(text) ==7:
+        license_plate_ = ''
+        mapping = {0: dict_char_to_int, 1: dict_char_to_int, 4: dict_char_to_int, 5: dict_char_to_int, 6: dict_char_to_int,
+                2: dict_char_to_int, 3: dict_char_to_int}
+        for j in [0, 1, 2, 3, 4, 5, 6]:
+            if text[j] in mapping[j].keys():
+                license_plate_ += mapping[j][text[j]]
+            else:
+                license_plate_ += text[j]
+    if len(text) == 8:
+        license_plate_ = ''
+        mapping = {0: dict_char_to_int, 1: dict_char_to_int, 4: dict_char_to_int, 5: dict_char_to_int, 6: dict_char_to_int,
+                7: dict_char_to_int,2: dict_char_to_int, 3: dict_char_to_int}
+        for j in [0, 1, 2, 3, 4, 5, 6, 7]:
+            if text[j] in mapping[j].keys():
+                license_plate_ += mapping[j][text[j]]
+            else:
+                license_plate_ += text[j]
+
+    return license_plate_
+
+
+def read_license_plate(license_plate_crop):
+    """
+    Read the license plate text from the given cropped image.
+
+    Args:
+        license_plate_crop (PIL.Image.Image): Cropped image containing the license plate.
+
+    Returns:
+        tuple: Tuple containing the formatted license plate text and its confidence score.
+    """
+    print("license_plate_crop", license_plate_crop)
+    detections = reader.readtext(license_plate_crop)
+    print("detections", detections)
+
+    text = ""
+    scores = []
+    for detection in detections:
+        _, t, score = detection
+        text = t + text
+        scores.append({"score":score, "len": len(t)})
+
+    t = ""
+    for char in text:
+        print(char)
+        if char in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or char.upper() in dict_char_to_int.keys():
+            print(char)
+            t += char.upper()
+    text = t
+    
+    score = 0
+    for s in scores:
+        score += s['score']*s['len']/len(text)
+
+    print("text", text)
+    print("score", score)
+
+    if license_complies_format(text):
+        return format_license(text), score
+
+    return None, None
+
+
+def get_car(license_plate, vehicle_track_ids):
+    """
+    Retrieve the vehicle coordinates and ID based on the license plate coordinates.
+
+    Args:
+        license_plate (tuple): Tuple containing the coordinates of the license plate (x1, y1, x2, y2, score, class_id).
+        vehicle_track_ids (list): List of vehicle track IDs and their corresponding coordinates.
+
+    Returns:
+        tuple: Tuple containing the vehicle coordinates (x1, y1, x2, y2) and ID.
+    """
+    x1, y1, x2, y2, score, class_id = license_plate
+
+    foundIt = False
+    for j in range(len(vehicle_track_ids)):
+        xcar1, ycar1, xcar2, ycar2, car_id = vehicle_track_ids[j]
+
+        if x1 > xcar1 and y1 > ycar1 and x2 < xcar2 and y2 < ycar2:
+            car_indx = j
+            foundIt = True
+            break
+
+    if foundIt:
+        return vehicle_track_ids[car_indx]
+
+    return -1, -1, -1, -1, -1
